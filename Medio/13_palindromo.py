d51f1ba0d5df37ee8ce77827feb4ed0f79915b4c
Almadieis <59013338+ElHereje@users.noreply.github.com>
2024-07-28 18:41:42 +0200

    Explicación código 13

--- Medio/13_palindromo.py
+++ Medio/13_palindromo.py
@@ -21,3 +21,50 @@ else:
     print('No lo es')
         
 
+# Este código en Python toma una frase del usuario, la procesa para eliminar los espacios y convertirla a minúsculas, 
+# y luego verifica si es un palíndromo (una palabra o frase que se lee igual de adelante hacia atrás y viceversa). 
+# Vamos a desglosar cada línea:
+
+# str1 = (input('Introduce un frase: ')).lower()
+
+# Aquí se solicita al usuario que introduzca una frase mediante la función input().
+# La frase introducida se convierte a minúsculas con el método .lower().
+# El resultado se almacena en la variable str1.
+
+# lisa = str1.replace(' ','')
+
+# Este paso elimina todos los espacios de la frase usando el método .replace(' ', '').
+# El resultado se almacena en la variable lisa.
+
+# lisa_invertida = lisa
+
+# Aquí se hace una copia de lisa y se asigna a lisa_invertida.
+
+# lisa = list(lisa)
+
+# La variable lisa, que es una cadena de texto, se convierte en una lista de caracteres.
+
+# lisa_invertida = list(lisa_invertida)
+
+# Similar al paso anterior, lisa_invertida se convierte en una lista de caracteres.
+
+# lisa_invertida.reverse()
+
+# La lista lisa_invertida se invierte usando el método .reverse(). Ahora contiene los mismos caracteres que 
+# lisa pero en orden inverso.
+
+# if lisa == lisa_invertida:
+
+# Se compara la lista lisa con lisa_invertida para ver si son iguales.
+
+# print('Es un palíndromo')
+
+# Si las listas son iguales, significa que la frase es un palíndromo, y se imprime "Es un palíndromo".
+
+# else:
+
+# Si las listas no son iguales, se ejecuta la siguiente instrucción.
+
+# print('No lo es')
+
+# Si las listas no son iguales, se imprime "No
