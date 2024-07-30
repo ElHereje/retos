622c24b82488ceee8df36c2ae38568e92c25063f
Almadieis <59013338+ElHereje@users.noreply.github.com>
2024-07-30 14:51:39 +0200

    intento de 14

---
+++ Facil/14_armstrong.py
@@ -0,0 +1,22 @@
+# /*
+#  * Escribe una función que calcule si un número dado es un número de Armstrong
+#  * (o también llamado narcisista).
+#  * Si no conoces qué es un número de Armstrong, debes buscar información
+#  * al respecto.
+#  */
+
+
+num = input('Introduzca nº a verificar: ')
+e = int(len(num))
+print(f'exponente {e}')
+
+res = 0
+for digito in num:
+    res = res + int(digito)**e
+    print(f'{digito} elevado a {e} --> acumulado = {res}')
+    
+
+if res == num:
+    print(f'{num} es un número de Armstrong.')
+else:
+    print(f'{num} NO es un número de Armstrong.')
